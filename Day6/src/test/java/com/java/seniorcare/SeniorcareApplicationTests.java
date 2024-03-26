54a77f303fb484729e7ca300c10c12317aad0bbe
yakshmin <120238607+yakshmin@users.noreply.github.com>
2024-03-26 11:11:10 +0530

    Add files via upload

---
+++ Day6/src/test/java/com/java/seniorcare/SeniorcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.seniorcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
